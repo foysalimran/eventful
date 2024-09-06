ee6fd9f4f7d61389fb83b039476769ec0c3f595e
sakhawat9 <sakhawathossain7969@gmail.com>
2024-09-06 09:21:32 +0600

    add new file

---
+++ src/Frontend/Frontend.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * The admin-facing functionality of the plugin.
+ *
+ * @link       https://themeatelier.net
+ * @since      1.0.0
+ *
+ * @package eventful
+ * @subpackage eventful/Frontend
+ * @author     ThemeAtelier<themeatelierbd@gmail.com>
+ */
+
+namespace ThemeAtelier\Eventful\Frontend;
+
+
+class Frontend {}
